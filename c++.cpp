b684fbe457dc1b68663eaaf887ced9bd64440961
Kubo-xen <abriansahd@gmail.com>
2026-02-14 08:36:14 +0700

    Add c++ and javascript file

---
+++ c++.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int number = 1;
+
+switch (number) {
+
+    case 1:
+    cout << ("We're inside case 1") << endl;
+    break;
+
+    case 2:
+    cout << ("We're inside case 2") << endl;
+    break;
+    
+    case 3:
+    cout << ("We'ere inside case 3") << endl;
+    break;
+    
+    case 4:
+    cout << ("We're inside case 4") << endl;
+    break;
+    
+    default:
+    cout << ("Default") << endl;
+}
+    return 0;
+}
---
+++ javascript.js
@@ -0,0 +1,23 @@
+var number = 1;
+
+switch (number) {
+
+    case 1:
+    console.log("We're inside case 1");
+    break;
+
+    case 2:
+    console.log("We're inside case 2");
+    break;
+
+    case 3:
+    console.log("We'ere inside case 3");
+    break;
+
+    case 4:
+    console.log("We're inside case 4");
+    break;
+
+    default:
+    console.log("Default");
+}
